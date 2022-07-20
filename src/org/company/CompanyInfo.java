0593aa64ec31f3c12620126c6da87db8d3abb798
magesh <saimagesh98@gmail.com>
2022-07-20 13:30:27 +0530

    company

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,15 @@
+package org.company;
+
+public class CompanyInfo {
+
+	private void companyName() {
+		System.out.println("infosis");
+	}	
+	
+public static void main(String[] args) {
+		
+		CompanyInfo e=new CompanyInfo();
+		e.companyName();
+}
+}
+
